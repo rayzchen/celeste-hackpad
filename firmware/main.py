47b2260310dcab7c5fc701c5902d08bcaeb1025e
Ray Chen <54078384+rayzchen@users.noreply.github.com>
2026-01-11 19:53:25 +0000

    Add RGB neopixel support

--- firmware/main.py
+++ firmware/main.py
@@ -1,27 +1,29 @@
 import board
-
 from kmk.kmk_keyboard import KMKKeyboard
-from kmk.scanners.keypad import KeyMatrix
 from kmk.keys import KC
+from kmk.scanners import DiodeOrientation
+from kmk.extensions.rgb import RGB, AnimationModes
+from kmk.extensions.media_keys import MediaKeys
 
 keyboard = KMKKeyboard()
+keyboard.extensions.append(MediaKeys())
 
-# macros = Macros()
-# keyboard.modules.append(macros)
-
-keyboard.matrix = KeyMatrix(
-    [board.D0, board.D1, board.D2],
-    [board.D7, board.D8, board.D10],
-    columns_to_anodes=True
-)
-
+keyboard.col_pins = (board.A0, board.A1, board.A2)
+keyboard.row_pins = (board.D7, board.D8, board.D10)
+keyboard.diode_orientation = DiodeOrientation.COL2ROW
 keyboard.keymap = [
-    [
-        KC.RIGHT, KC.DOWN, KC.UP, KC.LEFT,
-        KC.C, KC.X, KC.Z, KC.ESC
-    ]
+    [KC.RIGHT, KC.DOWN, KC.UP, KC.LEFT, KC.C, KC.X, KC.Z, KC.ENTER, KC.AUDIO_MUTE],
 ]
 
-# Start kmk!
+rgb = RGB(
+    pixel_pin=board.D9,
+    num_pixels=8,
+    val_limit=100,
+    breathe_center=1.5,
+    animation_mode=AnimationModes.BREATHING_RAINBOW,
+    animation_speed=2,
+)
+keyboard.extensions.append(rgb)
+
 if __name__ == '__main__':
     keyboard.go()
